64689589bc6812975bf3b9118f74b724d7dd34e5
Muqali <muqqli2022@gmail.com>
2023-04-03 22:14:58 +0800

    [修改]前台签到程序新增提现限制业务支持

--- app/api/controller/v1/sign.go
+++ app/api/controller/v1/sign.go
@@ -14,12 +14,12 @@ type SignController struct {
 	controller.AuthController
 }
 
-//	@Summary	签到
-//	@Tags		签到
-//	@Param		token	header		string			false	"用户令牌"
-//	@Param		object	query		request.Request	false	"查询参数"
-//	@Success	200		{object}	response.Response
-//	@Router		/sign/sign [get]
+// @Summary	签到
+// @Tags		签到
+// @Param		token	header		string			false	"用户令牌"
+// @Param		object	query		request.Request	false	"查询参数"
+// @Success	200		{object}	response.Response
+// @Router		/sign/sign [get]
 func (this SignController) Sign(c *gin.Context) {
 	member := this.MemberInfo(c)
 	today := common.GetTodayZero()
@@ -30,24 +30,78 @@ func (this SignController) Sign(c *gin.Context) {
 		return
 	}
 	global.REDIS.Set(key, 123, -1)
-	//送奖励 1块钱
-	member.TotalBalance += int64(model.UNITY)
-	member.UseBalance += int64(model.UNITY)
-	member.Update("total_balance","use_balance");
-	//记录奖励
-	trade := model.Trade{
-		UID:        member.ID,
-		TradeType:  22,
-		ItemID:     0,
-		Amount:     int64(model.UNITY),
-		Before:     member.UseBalance - int64(model.UNITY),
-		After:      member.UseBalance,
-		Desc:       "签到奖励",
-		CreateTime: time.Now().Unix(),
-		UpdateTime: time.Now().Unix(),
-		IsFrontend: 1,
+
+	//基础配置表
+	config := model.SetBase{}
+	config.Get()
+
+	//收盘状态分析
+	isRetreatStatus := common.ParseRetreatStatus(config.RetreatStartDate)
+	if isRetreatStatus == true {
+		if config.IncomeBalanceRate == 0 {
+			//默认为90%
+			config.IncomeBalanceRate = 9000
+		}
+		//送奖励 1块钱,可用余额,可提现余额分析
+		balanceAmount := int64(model.UNITY) * int64(config.IncomeBalanceRate) / int64(model.UNITY)
+		useBalanceAmount := int64(model.UNITY) - balanceAmount
+
+		member.TotalBalance += balanceAmount
+		member.Balance += balanceAmount
+		member.UseBalance += useBalanceAmount
+		member.Update("total_balance", "balance", "use_balance")
+
+		//记录奖励
+		trade := model.Trade{
+			UID:        member.ID,
+			TradeType:  22,
+			ItemID:     0,
+			Amount:     int64(model.UNITY),
+			Before:     member.Balance - balanceAmount,
+			After:      member.Balance,
+			Desc:       "签到奖励(可用余额)",
+			CreateTime: time.Now().Unix(),
+			UpdateTime: time.Now().Unix(),
+			IsFrontend: 1,
+		}
+		trade.Insert()
+
+		trade = model.Trade{
+			UID:        member.ID,
+			TradeType:  22,
+			ItemID:     0,
+			Amount:     int64(model.UNITY),
+			Before:     member.UseBalance - useBalanceAmount,
+			After:      member.UseBalance,
+			Desc:       "签到奖励(可提现余额)",
+			CreateTime: time.Now().Unix(),
+			UpdateTime: time.Now().Unix(),
+			IsFrontend: 1,
+		}
+		trade.Insert()
+
+	} else {
+		//送奖励 1块钱
+		member.TotalBalance += int64(model.UNITY)
+		member.UseBalance += int64(model.UNITY)
+		member.Update("total_balance", "use_balance")
+
+		//记录奖励
+		trade := model.Trade{
+			UID:        member.ID,
+			TradeType:  22,
+			ItemID:     0,
+			Amount:     int64(model.UNITY),
+			Before:     member.UseBalance - int64(model.UNITY),
+			After:      member.UseBalance,
+			Desc:       "签到奖励",
+			CreateTime: time.Now().Unix(),
+			UpdateTime: time.Now().Unix(),
+			IsFrontend: 1,
+		}
+		trade.Insert()
 	}
-	trade.Insert()
+
 	this.Json(c, 0, "签到成功", nil)
 	return
 }
--- common/time.go
+++ common/time.go
@@ -11,7 +11,7 @@ const YMD = "2006-01-02"
 
 var zone, _ = time.LoadLocation("Asia/Shanghai")
 
-//获取当前时间的年月日 时分秒
+// 获取当前时间的年月日 时分秒
 func GetTimeYMDS() string {
 	return time.Now().In(zone).Format(YMDS)
 }
@@ -80,4 +80,20 @@ func UnixByYearMonthDay(year, month, day int) int64 {
 	t := time.Now()
 	newTime := time.Date(year, time.Month(month), day, 0, 0, 0, 0, t.Location())
 	return newTime.Unix()
+}
+
+// 分析收盘状态
+func ParseRetreatStatus(startDate string) bool {
+	//参数分析
+	if startDate == "" {
+		return false
+	}
+	//收盘状态分析
+	closingStatus := false
+	startClosingTime, _ := time.ParseInLocation("2006-01-02", startDate, zone)
+	timeNow := time.Now().Unix()
+	if timeNow >= startClosingTime.Unix() {
+		closingStatus = true
+	}
+	return closingStatus
 }
